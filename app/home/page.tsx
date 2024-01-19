09e6c97a7a1ddc4363442d9728ed6642bfd8e535
Jackles 123 <jack.welsh@drake.edu>
2024-01-19 11:40:21 -0600

    db stuff again

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,30 @@
 import LikeButton from "../_components/like-button";
 import CreateLog from "../_components/CreateLog";
+import React from 'react';
+import { db, createData, readData, updateData, deleteData} from "../db.js"; 
+
+//Example usage of the database functions 
+const YourPage = () => {
+  const fetchData = async () => {
+      try {
+        const result = await createData(
+        'users', {'email':'bruh2@gmail.com', 
+        'prof_pic': 'bru.jpg',
+        'username': 'jack2'});
+        const userData = await readData('users');
+        console.log('Read Data:', userData);
+        
+        await updateData('users', {'email': 'jack@gmail.com'});
+        await deleteData('users');
+      } catch (error) {
+        console.error('Error writing data:', error);
+      }
+    };
+};
+
+//export default YourPage;
+
+
 
 export default function homePage() {
   return (
@@ -14,7 +39,8 @@ export default function homePage() {
       
       <LikeButton />
 
+
       {/*  */}
     </div>
   );
-}
+  };
